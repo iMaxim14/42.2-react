ae9003e92064d8f4285d649ac2f3e734ec73598d
iMaxim14 <i.maxim.prog@gmail.com>
2024-11-08 20:34:06 +0100

    lesson 2

---
+++ src/components/LessonCard/LessonCard.jsx
@@ -0,0 +1,16 @@
+import Button from "../Button/Button";
+import './styles.css';
+import {lessonData} from './data';
+
+function LessonCard(){
+
+    return(
+        <div className="lesson-card">
+            <h3>Lesson {lessonData.lesson_number}</h3>
+            <p>{lessonData.teacher}</p>
+            <Button/>
+        </div>
+    )
+};
+
+export default LessonCard;
---
+++ src/components/LessonCard/data.js
@@ -0,0 +1,4 @@
+export const lessonData = {
+    lesson_number: "2",
+    teacher: 'Ekaterina Zavertyaeva',
+};
