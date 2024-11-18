3ced631a9bab2ff83d1264a1464e7f8baf34d5b4
iMaxim14 <i.maxim.prog@gmail.com>
2024-11-18 20:11:11 +0100

    HW-4.Hooks. Likes, disslikes

---
+++ src/homeworks/Homework04/Homework04.jsx
@@ -0,0 +1,12 @@
+import Feedback from "../../components/Feedback/Feedback";
+import './styles.css'
+
+function Homework04() {
+  return (
+    <div className="hw04-container">
+      <Feedback />
+    </div>
+  );
+}
+
+export default Homework04;
